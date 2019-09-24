b1db26bd591a857e20756e5823ef161c0840130a
Pritpal Singh <psbrar2001@gmail.com>
2019-09-24 19:48:46 -0400

    initialized

---
+++ StudentSystem/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StudentSystem.Models;
+
+namespace StudentSystem.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ViewResult Index()
+        {
+            Student TestStudent = new Student() { FirstName = "Ram" };
+            return View(TestStudent);
+        }
+    }
+}
---
+++ StudentSystem/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSystem.Models
+{
+    public class Student
+    {
+        public string CourseCode { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public bool FullTime { get; set; }
+        public double Gpa { get; set; }
+        public string LastName { get; set; }
+        public int StudentId { get; set; }
+    }
+}
